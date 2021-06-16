a190eee0980d9131e16579680821adde663be718
Новиков Кирилл Сергеевич <ksnovikov@tsconsulting.com>
2021-06-16 20:05:30 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+// Boot express
+const app: Application = express();
+const port = 5000;
+const baseURL = "https://chess-samara.ru/library/problems/";
+
+// Application routing
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send("SUccess");
+});
+
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
